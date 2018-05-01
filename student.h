6d4acd16949cac4bf19d0699eeeb849af80f68e3
jkue1 <jkue1@mail.csuchico.edu>
2018-04-30 18:27:18 -0700

    done

---
+++ student.h
@@ -0,0 +1,21 @@
+
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
+#include <string>
+
+using namespace std;
+class student
+{
+    public:
+    string fullName();
+    void setName(string,string);
+    
+    private: 
+    string firstname;
+    string lastname;
+   
+    
+            
+};
+
+#endif
